383e8429397c8fad1b7cb508870ed149e52f8482
ziesop20 <sincerelysophie@hotmail.com>
2018-07-23 19:42:05 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -19,9 +19,10 @@ lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word.length # Put a . after big_word and use a String method.
+letter_count = big_word.length 
+# Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}.length"
+puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
